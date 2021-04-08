e890fa01087ec9f56650b84ae671753d8ee86ea6
Razvan Tudosie <tudosie.razvan@gmail.com>
2021-04-08 17:08:42 +0300

    added rounting history

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
     ? '/faculty-project/'
-    : '/'
+    : '/',
+
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = "MyMovies";
+                return args;
+            })
+    }
 }
