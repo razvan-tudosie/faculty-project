a349114dd4cede3c7f09bc28b6ad9f3cccfba895
Razvan Tudosie <tudosie.razvan@gmail.com>
2021-04-10 19:10:45 +0300

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
     ? '/faculty-project/'
-    : '/faculty-project/',
+    : '/',
 
     chainWebpack: config => {
         config
